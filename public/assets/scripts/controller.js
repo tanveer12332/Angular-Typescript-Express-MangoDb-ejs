42e4bfa37d908454d0361b4560c988b50d020b6f
tanveer <igm.tan@gmail.com>
2015-10-05 19:15:36 +0500

    update search function

--- public/assets/scripts/controller.js
+++ public/assets/scripts/controller.js
@@ -1,7 +1,7 @@
 var meanApp = angular.module('meanApp', ['ui.bootstrap']);
 
 meanApp.controller('AppCtrl', function($scope, $rootScope, $http, $modal, $log, myService){
- 
+ //start paging
  $scope.totalItems = 2;
  $scope.currentPage = 1;
 
@@ -12,14 +12,15 @@ meanApp.controller('AppCtrl', function($scope, $rootScope, $http, $modal, $log, 
   $scope.pageChanged = function() {
     $log.log('Page changed to: ' + $scope.currentPage);
   };
-	
+	//end paging
+  //start get request
 	$rootScope.contactList=[];
 	myService.refresh($http).then(function(data){
     $rootScope.contactList = data.data;
     $scope.contact = "";
   });
-  
-
+  //end
+ //open model popup
   $scope.showModel = function () {
    var  modalInstance = $modal.open({
       animation: $scope.animationsEnabled,
@@ -27,17 +28,18 @@ meanApp.controller('AppCtrl', function($scope, $rootScope, $http, $modal, $log, 
       controller: 'ModalInstanceCtrl'
      });
   };
-
+ //end model
+ //start remove user function
  $scope.remove = function(id){
-
 	 $http.delete('/delcontacts/' + id).success(function(responce){
-		myService.refresh($http).then(function(data){
+  		myService.refresh($http).then(function(data){
       $rootScope.contactList = data.data;
       $scope.contact= "";
     });
 	 })
  }
- 
+ //end 
+ //start edit function
   $scope.edit = function(id) {
     // console.log(id);
 	  $modal.open({
@@ -49,69 +51,73 @@ meanApp.controller('AppCtrl', function($scope, $rootScope, $http, $modal, $log, 
             },id: function() {
                 return id;
             }
-          
           }
-	     });
-       
-	   }
-     
-})
+	    });
+  }
+  //end edit function
 
+//end controller
+})
+//start search controller
+ meanApp.controller('searchCtrl', function($scope,$http, $rootScope){
+      $scope.userinput= "";
+      $scope.SearchFun = function(){
+      $http.get('/searchcontact/' + $scope.userinput).success(function(responce){
+        console.log(responce);
+        $rootScope.contactList = responce;
+        $scope.userinput= "";
+      });
+      }
+//end search controller 
+})
 
 meanApp.controller('ModalInstanceCtrl', function ($scope, $rootScope, $http, $modal, $modalInstance, myService, $timeout) {
   
   $scope.addContact = function(){
-	// console.log($scope.contact);
-	 $http.post('/setcontacts', $scope.contact).success(function(responce){
+	    // console.log($scope.contact);
+	    $http.post('/setcontacts', $scope.contact).success(function(responce){
 	    $modalInstance.close();
       myService.refresh($http).then(function(data){
       $rootScope.contactList = data.data;
       $scope.contact = "";
     });
 	 })
-  // $timeout(function() {
-     
-  //     console.log('Refresh function execute');
-  //  }, 2000)
-	// refresh();
- };
+  };
  
   $scope.cancel = function () {
     $modalInstance.dismiss('cancel');
   };
 
-     
+//end modal controller    
 });
 
 meanApp.controller('editModalInstanceCtrl', function ($scope, $rootScope, $http, $modal, $modalInstance, items, id, myService) {
     $scope.IsVisible = true;
-  $scope.cancel = function () {
+    $scope.cancel = function () {
     $modalInstance.dismiss('cancel');
   };
-  $scope.id=id;
-  $scope.items = $http.get('/editcontacts/'+ id).success(function(responce){
+    $scope.id=id;
+    $scope.items = $http.get('/editcontacts/'+ id).success(function(responce){
   
-	   $scope.contact = responce;
+	  $scope.contact = responce;
   });
  
-   $scope.update = function(){
+    $scope.update = function(){
      console.log($scope.contact._id);
      $http.put('/updatecontact/' + $scope.contact._id, $scope.contact).success(function(responce){
      $modalInstance.dismiss('cancel');
-      myService.refresh($http).then(function(data){
+     myService.refresh($http).then(function(data){
         $rootScope.contactList = data.data;
         $scope.contact = "";
-      });
+     });
      
      })
-   }
-     
+    }
+//end controll  
 });
 meanApp.controller('loginCtrl', function ($scope, $http,$window) {
-        
   $scope.usernamemsg = "";
   $scope.userpasmsg = "";
-  
   $scope.data= ['user@gmail.com','123'];
   
   //user email  focus function 
@@ -120,30 +126,28 @@ meanApp.controller('loginCtrl', function ($scope, $http,$window) {
     $scope.usernamemsg = "";
   };
   //user password  focus function 
-   $scope.userpassfocus = function(){
-    $scope.data.password = "";
-  $scope.userpasmsg = "";
+  $scope.userpassfocus = function(){
+   $scope.data.password = "";
+   $scope.userpasmsg = "";
   };
   
-$scope.login = function () {
-  if($scope.data.username ==  $scope.data[0]){
+  $scope.login = function () {
+    if($scope.data.username ==  $scope.data[0]){
     
-    if($scope.data.password == $scope.data[1]){
+      if($scope.data.password == $scope.data[1]){
         console.log('user is login');   
         $http.get('/dashbord').success(function(responce){
-              $window.location.href = '/dashbord';
+        $window.location.href = '/dashbord';
         });   
-    }else{
-      console.log('rong password');
-      $scope.userpasmsg = "Rong password";
-    }
-  } 
- 
-      else{
+         }else{
+        console.log('rong password');
+        $scope.userpasmsg = "Rong password";
+         }
+       } 
+       else{
         console.log('user name and password is rong');
         $scope.usernamemsg = "User name is not valid";
       }
-  
-}
+  }
 
 });
